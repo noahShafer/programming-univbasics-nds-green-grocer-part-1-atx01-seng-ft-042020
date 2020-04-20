c62b9518bb62edb3ba4cf4f7594fe1eb6bc2739a
github username <github email address>
2020-04-20 18:55:17 -0500

    Done.

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,17 +1,27 @@
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
-
+  res = nil
+  collection.each {|e| 
+    res = e if e[:item] == name
+    break if res != nil
+  }
+  return res
 end
 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-
+  res = []
+  item_names = cart.map {|e| e[:item]}.uniq
+  item_names.each {|name|
+    items = cart.select {|i| i[:item] == name}
+    items[0][:count] = items.count
+    res.append(items[0])
+  }
+  return res
 end
+p consolidate_cart([
+  {:item => "AVOCADO", :price => 3.00, :clearance => true },
+  {:item => "AVOCADO", :price => 3.00, :clearance => true },
+  {:item => "KALE", :price => 3.00, :clearance => false}
+])
 
 
   
